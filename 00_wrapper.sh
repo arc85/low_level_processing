f0e204107ddae1160a663cfc13cf1c59d82b60c2
Anthony Cillo <arc85@pitt.edu>
2020-08-10 20:00:44 -0400

    Change mkfastq to .sh in wrapper

--- 00_wrapper.sh
+++ 00_wrapper.sh
@@ -4,8 +4,8 @@ source 01_setup.sh
 source 02_run_download.sh
 source 03_untar_downloaded_run.sh
 source 04_generate_sample_sheet.sh
-sbatch 05_mkfastq.sbatch #change to srun in the future so the pipeline waits
+source 05_mkfastq.sh
 source 06_folder_conversion.sh
 source 07_define_align_array.sh
 sbatch --array=$ARRAY 08_align.sbatch
-#build 09_xxx.sh into sbatch to run when the array completes 
+#build 09_xxx.sh into sbatch to run when the array completes
