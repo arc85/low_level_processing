acc9002a0fae0e95a03e6a38b92941ceee1badb2
Anthony Cillo <arc85@pitt.edu>
2020-08-13 16:46:27 -0400

    Add job name and redirect stdout stderr to jobname.out

--- 00_wrapper.sh
+++ 00_wrapper.sh
@@ -4,7 +4,7 @@ source 01_setup.sh
 source 02_run_download.sh
 source 03_untar_downloaded_run.sh
 source 04_generate_sample_sheet.sh
-srun -n1 -t04:00:00 --cpus-per-task=4 --mem=40g 05_mkfastq.sh
+srun -n1 -t04:00:00 --cpus-per-task=4 --mem=40g --job-name mkfastq --output %x.out 05_mkfastq.sh
 source 06_folder_conversion.sh
 source 07_define_align_array.sh
 sbatch --array=$ARRAY 08_align.sbatch
