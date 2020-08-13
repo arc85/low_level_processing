e7137b6f88c6ab0662010ba7a83c1f671c4b745c
Anthony Cillo <arc85@pitt.edu>
2020-08-13 12:23:32 -0400

    Modify syntax for srun

--- 00_wrapper.sh
+++ 00_wrapper.sh
@@ -5,7 +5,6 @@ source 02_run_download.sh
 source 03_untar_downloaded_run.sh
 source 04_generate_sample_sheet.sh
 srun -n1 -t04:00:00 --cpus-per-task=4 --mem=40g 05_mkfastq.sh
-exit
 source 06_folder_conversion.sh
 source 07_define_align_array.sh
 sbatch --array=$ARRAY 08_align.sbatch
