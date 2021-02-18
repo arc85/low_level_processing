ec162b3c738c951e8e276ec3707f9d77d9d518be
Anthony Cillo <arc85@pitt.edu>
2021-02-17 23:44:14 -0500

    Create vars from reference csv

--- bin/01_setup.sh
+++ bin/01_setup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-export OVERALL_DIR='/Overall_run_directory/'
-export OVERALL_FOLDER='Run_folder'
-export RUN_FOLDER='Run_name'
-export FLOWCELL='Flowcell'
-export FASTQ_FOLDER='Fastq_folder'
-export MAT_FOLDER='matrix_folder'
-export CITESEQ_FOLDER="citeseq_folder"
-export TCR_BCR_FOLDER="tcr_bcr_folder"
-export USER_FOR_EMAIL='arc85@pitt.edu'
+while IFS= read -r line
+  do
+    declare $(echo "$line" | cut -d, -f1)=$(echo "$line" | cut -d, -f2)
+  done < variable_names.csv
