95c9dcba3ed9f7a406dcb12c7849616d91ed1fe0
Anthony Cillo <arc85@pitt.edu>
2021-02-18 12:09:13 -0500

    Define samples for GEX alignment

--- bin/07_define_align_array.sh
+++ bin/07_define_align_array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd $OVERALL_DIR/${FASTQ_FOLDER}/${FLOWCELL}
+R --vanilla -f ./lib/R/define_gex_samples.R
 
-TO_ALIGN=($(ls | grep -v "Hash\|hash\|CITEseq\|TCR\|BCR"))
+TO_ALIGN=$(cat ./input_files/gex_samples_for_array.csv)
 ALIGN_LENGTH=${#TO_ALIGN[@]}
 ALIGN_LENGTH=$(expr $ALIGN_LENGTH - 1)
 ARRAY="0-$ALIGN_LENGTH"
