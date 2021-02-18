9a5bda4a0e12e169f35f9da92ac2c8a3463d42ad
Anthony Cillo <arc85@pitt.edu>
2021-02-18 12:19:20 -0500

    Define and create citeseq arrays

--- bin/07b_define_citeseq_align_array.sh
+++ bin/07b_define_citeseq_align_array.sh
@@ -3,12 +3,12 @@
 module load  gcc/8.2.0
 module load r/3.6.0
 
-cd $OVERALL_DIR/${FASTQ_FOLDER}/${FLOWCELL}
+R --vanilla -f ./lib/R/define_citeseq_samples.R
 
-TO_ALIGN_CITESEQ=($(ls | grep "Hash\|hash\|CITEseq"))
-ALIGN_LENGTH_CITESEQ=${#TO_ALIGN[@]}
-ALIGN_LENGTH_CITESEQ=$(expr $ALIGN_LENGTH - 1)
-ARRAY_CITESEQ="0-$ALIGN_LENGTH"
+TO_ALIGN_CITESEQ=$(cat ./input_files/citeseq_samples_for_array.csv)
+ALIGN_LENGTH_CITESEQ=${#TO_ALIGN_CITESEQ[@]}
+ALIGN_LENGTH_CITESEQ=$(expr $ALIGN_LENGTH_CITESEQ - 1)
+ARRAY_CITESEQ="0-$ALIGN_LENGTH_CITESEQ"
 
 export TO_ALIGN_CITESEQ
 export ARRAY_CITESEQ
