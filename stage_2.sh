1c85c8a048553e0e1cc70e0487ff095afbf79ba7
Anthony Cillo <arc85@pitt.edu>
2021-02-19 09:45:32 -0500

    Add email and setup to stage 2

--- stage_2.sh
+++ stage_2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+source ./bin/01_setup.sh
 source ./bin/05_define_align_array.sh
-sbatch --array=$ARRAY ./bin/06_align.sbatch
+sbatch --array=$ARRAY --mail-user=$USER_FOR_EMAIL ./bin/06_align.sbatch
