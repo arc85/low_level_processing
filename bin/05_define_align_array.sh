5afcf907f0a06fac55df739fa1a46359d3cf6bba
Anthony Cillo <arc85@pitt.edu>
2021-02-19 09:55:19 -0500

    Create to_align array from sample info sheet

--- bin/05_define_align_array.sh
+++ bin/05_define_align_array.sh
@@ -5,7 +5,7 @@ module load r/3.6.0
 
 R --vanilla -f ./lib/R/define_gex_samples.R
 
-TO_ALIGN=$(cat ./input_files/gex_samples_for_array.csv)
+TO_ALIGN=($(cat ./input_files/gex_samples_for_array.csv))
 ALIGN_LENGTH=${#TO_ALIGN[@]}
 ALIGN_LENGTH=$(expr $ALIGN_LENGTH - 1)
 ARRAY="0-$ALIGN_LENGTH"
