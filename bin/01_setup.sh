4064bb703a208b8673b604e11a79ab051ca78b60
Anthony Cillo <arc85@pitt.edu>
2021-05-06 10:54:50 -0400

    Ensure properly formatting files from mac during setup

--- bin/01_setup.sh
+++ bin/01_setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Ensure input file formats are correct
+sed -i 's/\r//' ./input_files/variable_names.csv
+sed -i 's/\r//' ./input_files/Download_Links.csv
+sed -i 's/\r//' ./input_files/sample_info_sheet.csv
+
 while IFS= read -r line
   do
     declare $(echo "$line" | cut -d, -f1)=$(echo "$line" | cut -d, -f2)
